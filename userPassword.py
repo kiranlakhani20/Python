b51a43aaba7d5d5b3752ff3ac211abb8ed79c42d
kiranlakhani20 <55124757+kiranlakhani20@users.noreply.github.com>
2024-05-09 12:32:26 +0530

    Create userPassword.py

---
+++ userPassword.py
@@ -0,0 +1,8 @@
+if userInput == username
+   userInput = input("Password?\n")   
+    if userInput == "password":
+        print("Welcome!")
+    else:
+        print("That is the wrong password.")
+else:
+    print("That is the wrong username.")
